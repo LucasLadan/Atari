83a23179b29c5ca7220cd997c0e1ed4e569f77ae
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-06-12 21:16:36 -0600

    Enemy spawn time:
    Enemy will spawn after two seconds once the player hit SPACE BAR key.

--- Assets/Ahamed/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Ahamed/Scripts/Enemy/EnemySpawner.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -6,16 +8,24 @@ public class EnemySpawner : MonoBehaviour
 
      void Update()
      {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown (KeyCode.Space))
         {
-            EnemyAppear();
+            StartCoroutine(EnemyAppearing());
         }
+            
+        
         
      }
     // Update is called once per frame
-    void EnemyAppear()
+    public void EnemyAppear()
     {
         Vector2 enemySpawnZone = new Vector2(Random.Range (-7, 8.21f), Random.Range(-3.46f, -3.46f));
         Instantiate (_enemy, enemySpawnZone, Quaternion.identity); 
+    }
+
+    IEnumerator EnemyAppearing()
+    {
+        yield return new WaitForSeconds (2);
+        EnemyAppear ();
     }
 }
